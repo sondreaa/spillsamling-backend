0e3d099e793ca25b6200b8422c7f2da1e24fbd28
sondreaa <104071423+sondreaa@users.noreply.github.com>
2023-03-14 14:34:49 +0100

    first

--- schemas/index.js
+++ schemas/index.js
@@ -1,1 +1,5 @@
-export const schemaTypes = []
+
+import spill from './spill'
+
+
+export const schemaTypes = [spill]
---
+++ schemas/spill.js
@@ -0,0 +1,20 @@
+export default {
+    name: 'spill',
+    type: 'document',
+    title: 'Spill',
+    fields: [
+        {name: 'title',
+        type: 'string',
+        title: 'Spilltittel'
+    
+        },
+        {name: 'platform',
+        type: 'string',
+        title: 'Platform'
+        },
+        {name: 'physical',
+        type: 'boolean',
+        title: 'Physical (Y/n)'
+        }
+    ]
+}
